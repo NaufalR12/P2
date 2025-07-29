8001676f5b24a5ea28a692b63b3fa37c06f84bfe
NaufalR12 <naufal.rafid.mf@gmail.com>
2025-07-29 12:06:11 +0700

    resr

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 const { SitemapStream, streamToPromise } = require('sitemap');
 const { createWriteStream } = require('fs');
 
-const sitemap = new SitemapStream({ hostname: 'https://namasitus.netlify.app' });
+const sitemap = new SitemapStream({ hostname: 'https://djelajahpujoku.netlify.app/' });
 
 sitemap.write({ url: '/', changefreq: 'weekly', priority: 1.0 });
 sitemap.write({ url: '/tentang', changefreq: 'monthly', priority: 0.8 });
